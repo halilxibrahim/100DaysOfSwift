7f3676c415e892067892dd6c3f32ff053fe1b0cf
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2024-02-06 14:09:08 +0300

    variables

---
+++ day-001/Strings-integers .playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var age = 24
+age
+
+var turkeyPopulation = 8_000_000 // If you have large numbers, Swift lets you use underscores as thousands separators – they don’t change the number, but they do make it easier to read. For example:
+
+var number = 2
+number
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+
---
+++ day-001/Variables.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+
+var str = "Hello, playground"
+
+var message = "Good morning, Halil!"
+
+var plane = "Boing"
+
+var fighterJet = "F-35"
